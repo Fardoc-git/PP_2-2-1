e460f1816907f451e158f5b1bdc97081565ea0fa
Fardoc-git <163406460+Fardoc-git@users.noreply.github.com>
2024-06-20 20:05:01 +0300

    q

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,14 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru"
+              , new Car("gds", 0)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru"
+              , new Car("bsd", 4)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru"
+              , new Car("bidl", 23)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru"
+              , new Car("jdj", 10000000)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
